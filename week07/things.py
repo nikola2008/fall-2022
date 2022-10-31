f8bb9049141f78ae0a493ecf901d0e72b9f340fa
Niko Brdarski <nikolabrdarski26@gmail.com>
2022-10-31 11:32:40 -0400

    z

---
+++ week07/things.py
@@ -0,0 +1,7 @@
+
+print("xxxxxxxxxxx")
+for spaces in range(10,0,-1):
+    for col in range(0,spaces):
+        print(" ",  end="")
+    print("x")
+print("xxxxxxxxxxx")
---
+++ week08/things.py
@@ -0,0 +1,5 @@
+
+for spaces in range(0,10):
+    for col in range(0,spaces):
+        print(" ", end="")
+    print("x")
