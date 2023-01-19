94ef0a9cfe583dd8c893b8e948265630f21b5e69
Niko Brdarski <nikolabrdarski26@gmail.com>
2023-01-18 22:50:26 -0500

    Final Project

---
+++ Final Project/Final Projet.py
@@ -0,0 +1,20 @@
+answer = input("A two stroke engine takes what kind of gas? " )
+if answer == "Mixed" or answer == "mixed":
+    print ("You are correct Young one. ")
+else:
+    print("Incorect")
+answer = input("Who started Koenigsegg? ")
+if answer == "Christian von Koenigsegg":
+    print("Very Very Good ")
+    
+else:
+    print("Why did you not look it up? ")
+answer = input("How many layers are in a Big Mac? ")
+if answer == "3":
+    
+ print("WWWWWWWWWWWOOOOOOOOOOWWWWWWWWWWWW!!! Thats good ")
+else:
+    print("Count next time you eat one ")
+
+
+
